c61c74f4b3370551ffbab86287c1062910ed5693
Kristian Duda <kristianduda83@gmail.com>
2022-10-18 13:13:26 +0200

    tests

--- __tests__/utils/forms.test.ts
+++ __tests__/utils/forms.test.ts
@@ -2,8 +2,8 @@ import { describe, test } from '@jest/globals'
 import { readFile, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { cwd } from 'node:process'
-import { loadAndValidate } from '../../src'
 import { loadAndBuildXsd } from '../../src/utils/forms'
+import { loadAndValidate } from '../../src/utils/validation'
 
 describe('generate xsd', () => {
   test('generate xsd', async () => {
@@ -18,11 +18,23 @@ describe('generate xsd', () => {
     await writeFile(xsdPath, xsd)
   })
 
-  test('valid xsd', async () => {
-    const xsdSchemaPath = resolve(cwd(), 'forms', '00603481.dopravneZnacenie.sk', 'schema.generated.xsd')
+  test('generate xsd (allOf)', async () => {
+    const templatePath = resolve(cwd(), 'forms', 'test', 'template.xsd')
+    const xsdPath = resolve(cwd(), 'forms', 'test', 'schema.xsd')
+
+    const templateBuffer = await readFile(templatePath)
+    const jsonSchemaPath = resolve(cwd(), 'forms', 'test', 'schema.json')
+    const jsonSchemaBuffer = await readFile(jsonSchemaPath)
+
+    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString());
+    await writeFile(xsdPath, xsd)
+  })
+
+    test('valid xsd (allOf)', async () => {
+    const xsdSchemaPath = resolve(cwd(), 'forms', 'test', 'schema.xsd')
     const xsdSchemaBuffer = await readFile(xsdSchemaPath)
 
-    const jsonSchemaPath = resolve(cwd(), 'forms', '00603481.dopravneZnacenie.sk', 'schema.json')
+    const jsonSchemaPath = resolve(cwd(), 'forms', 'test', 'schema.json')
     const jsonSchemaBuffer = await readFile(jsonSchemaPath)
 
     const errors = loadAndValidate(xsdSchemaBuffer.toString(), JSON.parse(jsonSchemaBuffer.toString()))
