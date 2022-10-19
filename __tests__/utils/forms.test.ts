78535e3bb2c9a0d5dc20c3f90387728c5cda9aa5
Kristian Duda <kristianduda83@gmail.com>
2022-10-19 13:05:24 +0200

    tests

--- __tests__/utils/forms.test.ts
+++ __tests__/utils/forms.test.ts
@@ -14,7 +14,7 @@ describe('generate xsd', () => {
     const jsonSchemaPath = resolve(cwd(), 'forms', '00603481.dopravneZnacenie.sk', 'schema.json')
     const jsonSchemaBuffer = await readFile(jsonSchemaPath)
 
-    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString());
+    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString())
     await writeFile(xsdPath, xsd)
   })
 
@@ -26,15 +26,61 @@ describe('generate xsd', () => {
     const jsonSchemaPath = resolve(cwd(), 'forms', 'test', 'schema.json')
     const jsonSchemaBuffer = await readFile(jsonSchemaPath)
 
-    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString());
+    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString())
     await writeFile(xsdPath, xsd)
   })
 
-    test('valid xsd (allOf)', async () => {
+  test('valid xsd (allOf)', async () => {
     const xsdSchemaPath = resolve(cwd(), 'forms', 'test', 'schema.xsd')
     const xsdSchemaBuffer = await readFile(xsdSchemaPath)
 
     const jsonSchemaPath = resolve(cwd(), 'forms', 'test', 'schema.json')
+    const jsonSchemaBuffer = await readFile(jsonSchemaPath)
+
+    const errors = loadAndValidate(xsdSchemaBuffer.toString(), JSON.parse(jsonSchemaBuffer.toString()))
+    expect(errors).toHaveLength(0)
+  })
+
+  test('generate xsd (if then)', async () => {
+    const templatePath = resolve(cwd(), 'forms', 'kontajner', 'template.xsd')
+    const xsdPath = resolve(cwd(), 'forms', 'kontajner', 'schema.xsd')
+
+    const templateBuffer = await readFile(templatePath)
+    const jsonSchemaPath = resolve(cwd(), 'forms', 'kontajner', 'schema.json')
+    const jsonSchemaBuffer = await readFile(jsonSchemaPath)
+
+    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString())
+    await writeFile(xsdPath, xsd)
+  })
+
+  test('valid xsd (if then)', async () => {
+    const xsdSchemaPath = resolve(cwd(), 'forms', 'kontajner', 'schema.xsd')
+    const xsdSchemaBuffer = await readFile(xsdSchemaPath)
+
+    const jsonSchemaPath = resolve(cwd(), 'forms', 'kontajner', 'schema.json')
+    const jsonSchemaBuffer = await readFile(jsonSchemaPath)
+
+    const errors = loadAndValidate(xsdSchemaBuffer.toString(), JSON.parse(jsonSchemaBuffer.toString()))
+    expect(errors).toHaveLength(0)
+  })
+
+  test('generate xsd (if then, allOf)', async () => {
+    const templatePath = resolve(cwd(), 'forms', 'kontajner-allOf', 'template.xsd')
+    const xsdPath = resolve(cwd(), 'forms', 'kontajner-allOf', 'schema.xsd')
+
+    const templateBuffer = await readFile(templatePath)
+    const jsonSchemaPath = resolve(cwd(), 'forms', 'kontajner-allOf', 'schema.json')
+    const jsonSchemaBuffer = await readFile(jsonSchemaPath)
+
+    const xsd = loadAndBuildXsd(JSON.parse(jsonSchemaBuffer.toString()), templateBuffer.toString())
+    await writeFile(xsdPath, xsd)
+  })
+
+  test('valid xsd (if then, allOf)', async () => {
+    const xsdSchemaPath = resolve(cwd(), 'forms', 'kontajner-allOf', 'schema.xsd')
+    const xsdSchemaBuffer = await readFile(xsdSchemaPath)
+
+    const jsonSchemaPath = resolve(cwd(), 'forms', 'kontajner-allOf', 'schema.json')
     const jsonSchemaBuffer = await readFile(jsonSchemaPath)
 
     const errors = loadAndValidate(xsdSchemaBuffer.toString(), JSON.parse(jsonSchemaBuffer.toString()))
